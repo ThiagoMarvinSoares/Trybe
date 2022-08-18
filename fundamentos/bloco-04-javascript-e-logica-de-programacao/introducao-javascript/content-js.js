fdfb80cc63b79b625489500e841a1b05f0276598
Thiago Marvin <thiagomarvin.soares@gmail.com>
2022-08-18 16:23:17 -0300

    adicao e conclusao dos exercicios do conteudo

---
+++ fundamentos/bloco-04-javascript-e-logica-de-programacao/introducao-javascript/content-js.js
@@ -0,0 +1,42 @@
+// const myName = "Thiago Marvin";
+// let birthCity = "Belo Horizonte";
+// let birthYear = 2000;
+// birthYear = 1990;
+// birthCity = "Contagem";
+// console.log(myName, birthCity,birthYear);
+
+// const notaTecnico = 59;
+
+// if (notaTecnico >= 80) {    
+//     console.log("Parabens voce foi aprovado");
+// } else if (notaTecnico < 80 && notaTecnico >= 60) {
+//     console.log("VOce esta na lista de espera");
+// } else {
+//     console.log("voce foi reprovado.");
+// };
+
+// const currentHour = 23;
+// let message;
+
+// if (currentHour >= 22) {
+//     message = "nao deveriamos comer nada";
+// } else if (currentHour >= 18 && currentHour < 22) {
+//     message = "vamos jantar";
+// } else if (currentHour >= 14 && currentHour < 18) {
+//     message = "bolo pro cafe da tarde";
+// } else if (currentHour >= 11 && currentHour < 14) {
+//     message = "almoco";
+// } else {
+//     message = "cafe da manha";
+// }
+// console.log(message);
+
+// let weekDay = "sabado";
+
+// if (weekDay == "segunda-feira" || weekDay == "terca-feira" || weekDay == "quarta-feira" || weekDay == "quinta-feira" || weekDay == "sexta-feira") {
+//     console.log("mais um dia de aprendizado na trybe");
+// } else if (weekDay == "sabado" ||weekDay == "domingo") {
+//     console.log("dia de descanso merecido.");
+// } else {
+//     console.log("dia invalido");
+// }
