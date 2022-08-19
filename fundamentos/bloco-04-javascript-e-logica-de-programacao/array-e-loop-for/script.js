0ec0fece8e39cc617c311a3144a6de526e04ef49
Thiago Marvin <thiagomarvin.soares@gmail.com>
2022-08-19 18:53:43 -0300

    exercicio 3 feito

--- fundamentos/bloco-04-javascript-e-logica-de-programacao/array-e-loop-for/script.js
+++ fundamentos/bloco-04-javascript-e-logica-de-programacao/array-e-loop-for/script.js
@@ -1,13 +1,24 @@
 
-
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // for (let index = 0; index < numbers.length; index += 1) {
 //     console.log(numbers[index]);
 // }
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum =0;
 
-for (let soma of numbers) {
-    sum += numbers;
-   
-}
-console.log(sum);
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum =0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     sum += numbers[index];
+    
+// }
+
+// console.log(sum);
+
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     sum += numbers[index];
+// }
+// console.log(sum / numbers.length);
