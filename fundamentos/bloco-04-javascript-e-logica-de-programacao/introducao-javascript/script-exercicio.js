872d596a9d6d7dc1a998c06c53065ecd7a6d5ea9
Thiago Marvin <thiagomarvin.soares@gmail.com>
2022-08-18 23:12:19 -0300

    comeco programa 5

--- fundamentos/bloco-04-javascript-e-logica-de-programacao/introducao-javascript/script-exercicio.js
+++ fundamentos/bloco-04-javascript-e-logica-de-programacao/introducao-javascript/script-exercicio.js
@@ -1,26 +1,69 @@
-const a =5 , b = 5;
-let conta= "divisao";
+// const a =5 , b = 5;
+// let conta= "divisao";
 
-if (conta == "soma") {
-    console.log(a + b);
-} else if (conta == "subtracao") {
-    console.log(a - b);
-} else if (conta == "multiplicacao") {
-    console.log(a * b);
-} else if (conta == "divisao") {
-    console.log(a / b);
-} else if (conta == "modulo") {
-    console.log(a % b);
+// if (conta == "soma") {
+//     console.log(a + b);
+// } else if (conta == "subtracao") {
+//     console.log(a - b);
+// } else if (conta == "multiplicacao") {
+//     console.log(a * b);
+// } else if (conta == "divisao") {
+//     console.log(a / b);
+// } else if (conta == "modulo") {
+//     console.log(a % b);
+// }
+
+// // 2. programa  
+
+// const num1 = 3, num2 = 4;
+
+// if (num1 > num2) {
+//     console.log(num1);
+// } else if (num1 < num2) {
+//     console.log(num2);
+// } else {
+//     console.log("numeros iguais");
+// }
+
+//3 programa
+
+// const num3 = 5, num4 = 6, num5 = 7;
+
+// if (num3 > num4 && num3 > num5) {
+//     console.log(num3);
+// } else if (num3 < num4 && num4 > num5) {
+//     console.log(num4);
+// } else if (num5 > num3 && num5 > num4) {
+//     console.log(num5);
+// } else {
+//     console.log("nao é um numero");
+// }
+
+//4 programa
+
+// let valor;
+
+// valor = 0;
+// if (valor > 0) {
+//     console.log("positive");
+// } else if (valor < 0) {
+//     console.log("negative");
+// } else {
+//     console.log("your number is 0");
+// }
+
+//5 programa
+
+const x = 60, y = -2, z = 60;
+
+if (x + y + z == 180) {
+    console.log("true");
+} else if (x + y + z != 180) {
+    console.log("false");
+} 
+else {
+    console.log("invalido");
 }
 
-// 2. programa  
+//6 programa
 
-const num1 = 3, num2 = 4;
-
-if (num1 > num2) {
-    console.log(num1);
-} else if (num1 < num2) {
-    console.log(num2);
-} else {
-    console.log("numeros iguais");
-}
