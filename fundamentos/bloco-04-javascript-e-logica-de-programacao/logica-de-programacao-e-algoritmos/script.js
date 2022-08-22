1d76a178308254b2ff6efe81fc8786ed13c5a7ab
Thiago Marvin <thiagomarvin.soares@gmail.com>
2022-08-22 18:12:30 -0300

    reverse done

--- fundamentos/bloco-04-javascript-e-logica-de-programacao/logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-04-javascript-e-logica-de-programacao/logica-de-programacao-e-algoritmos/script.js
@@ -1,9 +1,18 @@
 //fatorial
-let num = 10;
-let cnt = 1;
-for (let index = 1; index < num; index += 1) {
-    cnt *= index;
+// let num = 10;
+// let cnt = 1;
+// for (let index = 1; index < num; index += 1) {
+//     cnt *= index;
     
-}
-console.log(cnt);
+// }
+// console.log(cnt);
 
+//reverse
+
+// let word = 'Tryber';
+// let sum = "";
+
+// for (var index = word.length -1;index >= 0; index-= 1) {
+//     sum += word[index];
+// }
+// console.log(sum);
